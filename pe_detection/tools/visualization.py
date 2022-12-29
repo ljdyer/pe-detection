dcc72aa290c51a8639c0d9230ba0a47cba4a1024
Laurence Dyer <ljdyer@gmail.com>
2022-12-29 07:45:06 +0000

    new

--- pe_detection/tools/visualization.py
+++ pe_detection/tools/visualization.py
@@ -10,7 +10,18 @@ from typing import Optional
 # ====================
 def token_counts_histogram(token_counts_df: pd.DataFrame,
                            ignore_cols: Optional[list] = None):
+    """Display a histogram showing distribution of token counts for all
+    columns in token counts DataFrame.
 
+    Args:
+      token_counts_df (pd.DataFrame):
+        A DataFrame containing token counts.
+      ignore_cols (Optional[list], optional):
+        A list of columns to ignore. Defaults to None.
+    """
+
+    if ignore_cols is None:
+        ignore_cols = []
     cividis = get_cmap('cividis')  
     token_counts_df = token_counts_df.drop(columns=ignore_cols)
     min_tokens = token_counts_df.min().min()
@@ -22,7 +33,7 @@ def token_counts_histogram(token_counts_df: pd.DataFrame,
     norm_tok_col_colors = [cividis(x) for x in linspace(0.5, 1, num=len(norm_tok_cols), endpoint=False)]
     col_colors = {col: color for col, color in list(zip(norm_cols, norm_col_colors)) + list(zip(norm_tok_cols, norm_tok_col_colors))}    
     bins = list(range(floor(min_tokens/10)*10, ceil(max_tokens/10)*10, 10))
-    fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(20, 20))
+    _, axes = plt.subplots(nrows=2, ncols=1, figsize=(20, 20))
     legend_patches = []
     for col in token_counts_df.columns:
         token_counts_df[col].hist(ax=axes[0], color=col_colors[col], bins=bins)
