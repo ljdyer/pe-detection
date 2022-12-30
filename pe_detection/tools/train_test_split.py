65c163f4183699f66005d3fea3f56fe97bcf8641
Laurence Dyer <ljdyer@gmail.com>
2022-12-30 08:55:35 +0000

    0.0031

--- pe_detection/tools/train_test_split.py
+++ pe_detection/tools/train_test_split.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from typing import Dict, Tuple, List
+from typing import Dict, Tuple, List, Optional
 from pe_detection.tools.text_helper import num_tokens
 
 
@@ -16,7 +16,9 @@ def get_doc_token_counts(df: pd.DataFrame, col_label: str) -> Dict[int, int]:
 
 # ====================
 def best_split(doc_token_counts: Dict[int, int],
-               desired_test_ratio: float) -> Tuple[List[int], List[int], float]:
+               desired_test_ratio: float,
+               must_be_train: Optional[List[int]] = None
+               ) -> Tuple[List[int], List[int], float]:
     """Work out train/test split that gets ratio of tokens as close as possible to 0.2"""
 
     num_docs = len(doc_token_counts.keys())
@@ -28,10 +30,26 @@ def best_split(doc_token_counts: Dict[int, int],
         bin_ = bin(i)[2:].zfill(num_docs)
         train = [doc_idx for doc_idx in range(num_docs) if bin_[doc_idx] == '0']
         test = [doc_idx for doc_idx in range(num_docs) if bin_[doc_idx] == '1']
+        if any([doc_idx in must_be_train for doc_idx in test]):
+            continue
         test_toks = sum([doc_token_counts[x] for x in test])
         actual_ratio = test_toks / total_toks
         splits.append((test, train, actual_ratio))
     return min(splits, key=lambda x: abs(x[2] - desired_test_ratio))
+
+
+# ====================
+def n_best_splits(doc_token_counts: Dict[int, int],
+                  desired_test_ratio: float,
+                  n: int) -> List[Tuple[List[int], List[int], float]]:
+
+    best_splits = []
+    must_be_train = []
+    for _ in n:
+        next_best = best_split(doc_token_counts, desired_test_ratio, must_be_train)
+        best_splits.append(next_best)
+        must_be_train.extend(next_best[0])
+    return best_splits
 
 
 # ====================
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ REQUIREMENTS = [
 
 setup(
     name='pe_detection',
-    version='0.0030',
+    version='0.0031',
     description="""\
 Tools for PE detection work.
 """,
