c2c5ef8d0de9daf31f3cd2c8ac48acf0894d3cc2
Laurence Dyer <ljdyer@gmail.com>
2022-12-30 18:58:05 +0000

    0.0047

--- version_up_and_push.py
+++ version_up_and_push.py
@@ -4,7 +4,7 @@ import subprocess
 with open('setup.py', 'r') as f:
     setup_py = f.read()
 ver_number = re.findall(r"version\='(.*)'", setup_py)[0]
-new_ver_number = str(float(ver_number)+0.0001)
+new_ver_number = f"{float(ver_number)+0.0001:.4f}"
 setup_py = setup_py.replace(str(ver_number), new_ver_number)
 with open('setup.py', 'w') as f:
     f.write(setup_py)
