86ca475f425b486b7bf5a4dcb7e4c387ceda7c09
Laurence Dyer <ljdyer@gmail.com>
2022-12-31 16:00:32 +0000

    0.0059

---
+++ pe_detection/tools/pandas_helper.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from contextlib import contextmanager
+from typing import List, Tuple
+
+
+# ====================
+@contextmanager
+def pandas_options(options: List[Tuple]):
+
+    before = [pd.get_option(option_name) for option_name, _ in options]
+    for option in options:
+        pd.set_option(*option)
+    yield
+    for option_idx, option in enumerate(options):
+        pd.set_option(option[0], before[option_idx])
+
+
+# ====================
+@contextmanager
+def show_all_rows():
+
+    with pandas_options('display.max_rows', None):
+        yield
