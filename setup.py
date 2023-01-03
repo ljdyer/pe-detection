926b28c9d11e5b0bc0b633f435caefc5a8cbb13e
Laurence Dyer <ljdyer@gmail.com>
2023-01-03 07:48:19 +0000

    0.0092

--- setup.py
+++ setup.py
@@ -7,18 +7,23 @@ REQUIREMENTS = [
     'pandas',
     'requests',
     'scikit-learn==1.0.2',
-    'more_itertools'
+    'more_itertools',
+    'spacy'
 ]
 
 setup(
     name='pe_detection',
-    version='0.0091',
+    version='0.0092',
     description="""\
 Tools for PE detection work.
 """,
     author='Laurence Dyer',
     author_email='ljdyer@gmail.com',
     url='https://github.com/ljdyer/pe-detection',
-    packages=['pe_detection.tools', 'pe_detection.learn.naive_bayes'],
+    packages=[
+        'pe_detection.tools',
+        'pe_detection.learn',
+        'pe_detection.preprocessing'
+    ],
     install_requires=REQUIREMENTS
 )
